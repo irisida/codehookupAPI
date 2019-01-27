969bd123f8a31e6193f3f89fd029bfc0e28a3c39
irisida <20445648+irisida@users.noreply.github.com>
2019-01-27 17:42:41 +0000

    first drop

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace matcher.API.Controllers
+{
+    /**
+     * default action here is where the [controller] assumes the
+     * name of the controller file.
+     * default location for this example here is http://localhost:5000/api/values
+     *
+     */
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2", "values3" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Matching App API
+
+The purpose is to demonstrate a walking skeleton (or better) for a matching/dating application. The backend approach will be DB->ORM-API.
+
+## More info
+
+The construct technical detail at a high level is:
+
+    DB  -> SQLite
+    ORM -> Entity Framework
+    API -> asp.net
